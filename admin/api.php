cc79ed1e7b53ad5448d0139984691cb9262c7d8c
hanifdinataa <hanifdinata24@gmail.com>
2024-12-21 10:11:31 +0700

    Users

--- admin/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config.php'; // Pastikan file koneksi diatur dengan benar
-
-header('Content-Type: application/json');
-
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($result);
-?>
